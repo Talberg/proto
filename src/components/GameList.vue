022a2b2cd98521236df86d2ce3a9d57cbcf26364
Alexander Talberg <alexandermtalberg@gmail.com>
2021-11-09 10:17:42 -0700

    route

---
+++ src/components/GameList.vue
@@ -0,0 +1,29 @@
+<template>
+<div class='col'  >
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextLog',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html, body {
+  height: 100%;
+}
+.col{
+    
+    
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(219, 21, 21);
+}
+
+</style>
